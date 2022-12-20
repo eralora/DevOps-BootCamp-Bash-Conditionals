b768710ff15ece2b94bfb3e14f2d41af18083c71
Yerassyl Tokenov <erasyl.tokenov.94@mail.ru>
2022-12-20 13:09:12 +0600

    finished task 1

--- cond1.sh
+++ cond1.sh
@@ -3,3 +3,20 @@
 IFS=',' read -r -a array <<< "$1"
 
 #Write your code here
+res=0
+
+for i in ${array[@]}; do
+	rem=$(( $i % 2 ))
+	
+	if [ $rem -eq 0 ]; then
+		res=$((res + i))
+	fi
+
+done
+
+echo $res
+
+
+ 
+
+

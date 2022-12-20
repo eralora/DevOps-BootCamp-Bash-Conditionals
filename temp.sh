e1d30de28640b3f57ef879e150b5673e1dc2be47
Yerassyl Tokenov <erasyl.tokenov.94@mail.ru>
2022-12-20 13:50:18 +0600

    task 2 finished

--- temp.sh
+++ temp.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 # Place your code here
+
+arg=$*
+
+len=${#arg}
+l=$((len-1))
+res=""
+scale=${arg:$l:1}
+
+if [[ $scale == 'C' ]]; then
+	num=${arg%C}
+	res=$(( num + 273 ))K
+elif [[ $scale == "K" ]]; then
+	num=${arg%K}
+	res=$(( num - 273 ))C
+fi
+
+echo $res

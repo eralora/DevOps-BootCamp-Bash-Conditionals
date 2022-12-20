a601011caa7d0e52c58c0f55fb26cc4a6df3b782
Yerassyl Tokenov <erasyl.tokenov.94@mail.ru>
2022-12-20 15:24:26 +0600

    Task 3 finished

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 # Place your code here
+
+str=$*
+
+alpha=${str//[^[:alpha:]]/}
+dig=${str//[^[:digit:]]/}
+sym=${str//[^*!@#$%^&()_+]/}
+echo Numbers: ${#dig} Symbols: ${#sym} Letters: ${#alpha}
